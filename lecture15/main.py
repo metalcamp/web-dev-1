8e38686561c19be4f69c84f2bc5d460ea42aba55
Mario Kranjec <mariokranjec91@gmail.com>
2020-06-08 18:38:09 +0200

    add lecture 15

---
+++ lecture15/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about-me")
+def about_me():
+    return render_template("about-me.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
+# @app.route("/hello")
+# def index():
+#     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
